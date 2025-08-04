b17a3225ffc24aa19ca9809ffe38416e9161cf5e
arthurescola <152647480+arthurescola@users.noreply.github.com>
2025-08-04 15:24:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,49 +6,49 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Você está ao lado de uma linha de um bonde com uma alavanca, se você a puxar, o bonde irá trocar de trajetório. No trilho em que o bonde está indo, está o seu melhor amigo amarrado. No segundo trilho, está 5 pessoas desconhecidas amarradas. O que você faz?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmação"
+                texto: "Puxa a alavanca, salvando seu melhor amigo.",
+                afirmacao: "...você preferiu sacrificar 5 desconhecidos para salvar seu amigo, "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
+                texto: "Não faz nada, salvando as 5 pessoas desconhecidas.",
+                afirmacao: "...você prefiriu sacrificar seu melhor amigo para salvar 5 pessoas que você não conhece, "
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Na mesma situação. Você encontra uma família no trilho do bonde, e no outro trilho, uma familia de cachorros. O que você faz?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "afirmação"
+                texto: "Puxa a alavanca, salvando a família.",
+                afirmacao: "preferiu salvar humanos sobre animais, "
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "afirmação"
+                texto: "Não faz nada, salvando a família de cachorros.",
+                afirmacao: "preferiu salvar animais sobre humanos, "
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "No mesmo problema. Você encontra um gato no trilho do bonde, e no segundo trilho, 5 milhões de camarões. O que você faz?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "afirmação"
+                texto: "Puxa a alavanca, salvando o gato.",
+                afirmacao: "preferiu salvar 1 animal doméstico sobre 5 milhões animais marinhos, "
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "afirmação"
+                texto: "Não faz nada, salvando os camarões.",
+                afirmacao: "preferiu salvar 5 milhões animais marinhos sobre 1 animal doméstico, "
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Nos primeiro trilho, você observa um bebê, que no futuro irá crescer para ser um cientista e irá criar a cura para o câncer, e no outro trilho, outro bebê que irá crescer um presidente que criará várias guerras mundiais. Mas uma multidão está olhando para você, e não sabem o futuro dos bebês. O que você faz?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                texto: "Puxa a alavanca, salvando o mundo do câncer, mas sendo visto como um assassino.",
                 afirmacao: "afirmação"
             },
             {
@@ -72,21 +72,21 @@ const perguntas = [
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta() {
     if (atual >= perguntas.length) {
-      mostraResultado();
-      return;
+        mostraResultado();
+        return;
     }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
     mostraAlternativas();
-  }
-  
+}
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
@@ -99,14 +99,15 @@ function mostraAlternativas(){
 
 function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = opcaoSelecionada.afirmacao;
-    historia += afirmacoes + "";
+    historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "No problema do Bonde...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
+
 mostraPergunta();
